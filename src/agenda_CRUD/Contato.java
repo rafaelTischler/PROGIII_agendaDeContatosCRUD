b734916f3c86aec2dc7c83506268f4bfc1a5a23b
Rafael Müller Tischler <rtischler@svs.iffarroupilha.local>
2024-09-30 11:33:19 -0300

    add classes Arquivo e Contato

---
+++ src/agenda_CRUD/Contato.java
@@ -0,0 +1,56 @@
+package agenda_CRUD;
+
+public class Contato {
+	private String nome;
+	private String apelido;
+	private String numero;
+	private String email;
+
+	public Contato() {
+
+	}
+
+	public Contato(String nome, String apelido, String numero, String email) {
+		this.nome = nome;
+		this.apelido = apelido;
+		this.email = email;
+		this.numero = numero;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public String[] toLinha() {
+		return new String[] { this.nome, this.apelido, this.numero, this.email };
+	}
+
+}
